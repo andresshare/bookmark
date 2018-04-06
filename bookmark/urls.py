37c3a71b0aad60a725b077ffc37e860f9c4a43e6
andresshare <andresshare@gmail.com>
2018-04-05 23:44:11 -0500

    modified views and urls

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import  url
 
 urlpatterns = [
     url(r'^all/$', view=views.AllBookmarksView.as_view()),
-    url(r'^detail-page/(?P<username>[\w]+)/$',view=views.bookmark_by_user,name='detail-route')
+    url(r'^detail-page/(?P<username>[\w]+)/$',view=views.BookmarkByUser.as_view(),name='detail-route')
 
 
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse,HttpResponseNotFound,Http404
 from bookmark.models import Bookmark
 
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+
 
 class AllBookmarksView(ListView):
     model = Bookmark
@@ -17,16 +18,29 @@ class AllBookmarksView(ListView):
 
 
 #def all_bookmarks(request):
- #   bookmarks = Bookmark.objects.filter(ispublic=True)
-  #  return render(request, 'bookmark/all_bookmarks.html',{'bookmarks':bookmarks})
+#   bookmarks = Bookmark.objects.filter(ispublic=True)
+#  return render(request, 'bookmark/all_bookmarks.html',{'bookmarks':bookmarks})
 
 #def  bookmark_detail(request, bookmark_id):
-   # if bookmark_id == '0':
-    #    return HttpResponseNotFound("Not Found")
-   # else:
-    #    return HttpResponse("bookmark id=%s" % bookmark_id)
+# if bookmark_id == '0':
+#    return HttpResponseNotFound("Not Found")
+# else:
+#    return HttpResponse("bookmark id=%s" % bookmark_id)
 
-def  bookmark_by_user(request, username):
-    user = get_object_or_404(User, username=username)
-    bookmarks = Bookmark.objects.filter(user=user)
-    return render(request,'bookmark/detail.html', {'bookmarks':bookmarks,'user':'user'})
+#def  bookmark_by_user(request, username):
+#   user = get_object_or_404(User, username=username)
+#  bookmarks = Bookmark.objects.filter(user=user)
+# return render(request,'bookmark/detail.html', {'bookmarks':bookmarks,'user':'user'})
+
+class BookmarkByUser(DetailView):
+    model = User
+    template_name ='bookmark/detail.html'
+    content_objects_name = 'user'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(User,username=self.kwargs['username'])
+
+    def get_context_data(self, **kwargs):
+        context = super(BookmarkByUser,self).get_context_data(**kwargs)
+        context['bookmarks'] = Bookmark.objects.filter(user=self.get_object(),ispublic=True)
+        return context
