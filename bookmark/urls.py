53cb76ecb05adec6957157f5f2fdbe56925bfc30
andresshare <andresshare@gmail.com>
2018-04-05 23:17:16 -0500

    modified views

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,8 +1,10 @@
 from bookmark import views
 from django.conf.urls import  url
 
+
 urlpatterns = [
-    url(r'^all/$', view=views.all_bookmarks),
+    url(r'^all/$', view=views.AllBookmarksView.as_view()),
     url(r'^detail-page/(?P<username>[\w]+)/$',view=views.bookmark_by_user,name='detail-route')
+
 
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,12 +1,24 @@
+
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse,HttpResponseNotFound,Http404
 from bookmark.models import Bookmark
-from django.contrib.auth.models import User
 
-def all_bookmarks(request):
-    bookmarks = Bookmark.objects.filter(ispublic=True)
-    return render(request, 'bookmark/all_bookmarks.html',{'bookmarks':bookmarks})
+from django.views.generic import ListView
+
+class AllBookmarksView(ListView):
+    model = Bookmark
+    template_name ='bookmark/all_bookmarks.html'
+    context_object_name ='bookmarks'
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(ispublic=True)
+
+
+#def all_bookmarks(request):
+ #   bookmarks = Bookmark.objects.filter(ispublic=True)
+  #  return render(request, 'bookmark/all_bookmarks.html',{'bookmarks':bookmarks})
 
 #def  bookmark_detail(request, bookmark_id):
    # if bookmark_id == '0':
